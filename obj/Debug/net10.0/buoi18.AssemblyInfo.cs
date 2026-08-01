c65a1736e1232d8636a20e63585987524c82c89a
Anne Tran <tranngphanh@gmail.com>
2026-08-01 23:54:26 +0700

    update

--- obj/Debug/net10.0/buoi18.AssemblyInfo.cs
+++ obj/Debug/net10.0/buoi18.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("buoi18")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7cc25f4b5add46fe2e7a91aa2c2de3ab9153b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("buoi18")]
 [assembly: System.Reflection.AssemblyTitleAttribute("buoi18")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
